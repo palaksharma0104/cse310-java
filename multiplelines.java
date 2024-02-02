4d2b6a9d9b14396c4af0a5f86c653b398ca922a7
palaksharma0104 <117135999+palaksharma0104@users.noreply.github.com>
2024-02-02 16:36:07 +0530

    Printing multiple lines of text

---
+++ multiplelines.java
@@ -0,0 +1,7 @@
+public class multiplelines {
+    public static void main(String args[]){
+        System.out.println("Hey!");
+        System.out.println("It's the next line hehe");
+    }
+    
+};
