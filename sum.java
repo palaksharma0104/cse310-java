54bec409aac4cbc128850b78e2f68c3bec8463e0
palaksharma0104 <117135999+palaksharma0104@users.noreply.github.com>
2024-02-04 23:18:15 +0530

    Printing the sum of 3 digits

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String args[]){
+        int num1 = 5; 
+        int num2 = 10;
+        int num3= 76; 
+        int sum = num1+num2+num3;
+        System.out.println("The sum of three numbers is : " +sum);
+       }
+    
+}
