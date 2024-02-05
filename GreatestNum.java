5905adee484b1e79a80ea67aabe18cf8f9d27023
palaksharma0104 <117135999+palaksharma0104@users.noreply.github.com>
2024-02-05 21:35:46 +0530

    Printing the largest number

---
+++ GreatestNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class GreatestNum{
+    public static void main(String args[]){
+        System.out.println("Enter any three numbers!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        int c = sc.nextInt();
+        if(a>=b && b>=c)
+        System.out.println("The greatest number is: "+a);
+        if(b>=c && b>=a)
+        System.out.println("The greatest number is: "+b);
+        else
+        System.out.println("The greatest number is: "+c);
+    }
+}
