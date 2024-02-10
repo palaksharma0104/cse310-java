67f6af0dc91873d114f41c895eeeeb8df51a79a3
palaksharma0104 <117135999+palaksharma0104@users.noreply.github.com>
2024-02-10 21:58:05 +0530

    Printing the table of an user input number

---
+++ Table.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Table {
+    public static void main(String args[]){
+        System.out.println("Choose a number whose table you want to see, make sure the number is less than 10!");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        if(num>=10){
+            for(int i =1; i<=10; i++){
+                int mul = num*i;
+                System.out.println(num + " x " +i+ " = " +mul);
+            }
+        }
+
+    }
+    
+}
