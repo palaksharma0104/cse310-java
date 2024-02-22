45c059f5f0089b23ab00b89c464217597e8b1b88
palaksharma0104 <117135999+palaksharma0104@users.noreply.github.com>
2024-02-22 22:24:24 +0530

    Mutiplying two float numbers

---
+++ MulFloat.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class MulFloat{
+    public static void main(String argsp[]){
+        System.out.println("Enter any two decimal numbers");
+        Scanner in = new Scanner(System.in);
+        float a = in.nextFloat();
+        float b = in.nextFloat();
+        float mul = a*b;
+        System.out.println("The product of the the numbers is: "+mul);
+        }
+}.
