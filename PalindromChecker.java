94f3d28d8e5c29af8f56e948fa0422a2da4407af
Palak Sharma <117135999+palaksharma0104@users.noreply.github.com>
2024-02-25 01:44:33 +0530

    Checking whether a string is a Palindrome or not

---
+++ PalindromChecker.java
@@ -0,0 +1,26 @@
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        String str = "radar"; // Change this string to test
+        
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+    
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
